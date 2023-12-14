07bbde908ac185a170f563df9cf0902e0e355c47
aravind <aravind16081996@gmail.com>
2023-12-14 17:07:59 +0530

    third

--- variables.sh
+++ variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-person1=ramesh
-person2=suresh
+person1=ram
+person2=bheem
 echo "$person1: hello $person2,good morning"
 echo "$person2: hi $person1,very good morning"
 echo "$person1: how are you,$person2"
