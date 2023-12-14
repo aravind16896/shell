be4d3d6c6d3ef84aa0495201b8e23904e127b751
aravind <aravind16081996@gmail.com>
2023-12-14 17:04:37 +0530

    second

--- variables.sh
+++ variables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-echo "ramesh: hello suresh,good morning"
-echo "suresh: hi ramesh,very good morning"
-echo "ramesh: how are you,suresh"
-echo "suresh: i am good ramesh,how are you"
+person1=ramesh
+person2=suresh
+echo "$person1: hello $person2,good morning"
+echo "$person2: hi $person1,very good morning"
+echo "$person1: how are you,$person2"
+echo "$person2: i am good $person1,how are you"
